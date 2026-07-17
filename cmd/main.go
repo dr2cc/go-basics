4c20a5a2b4dab206ebd625b99c48e3c4b20d0903
Constantine <tmail99@gmail.com>
2026-07-17 17:16:49 +0300

    chapter05

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+type employee struct {
+	id     int
+	name   string
+	age    int
+	salary int
+}
+
+type storage interface {
+	insert(e employee) error
+	get(id int) (employee, error)
+	delete(id int) error
+}
+
 func main() {
 
 }
