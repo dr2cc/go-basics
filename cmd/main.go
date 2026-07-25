be160e3018e73cff74807a59b7919e842abc680a
Constantine <tmail99@gmail.com>
2026-07-25 14:27:03 +0300

    minor

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,18 @@ package main
 
 import (
 	"fmt"
-	"go_basics/storage"
 )
 
 func main() {
-	ms := storage.NewMemoryStorage()
-	ds := storage.NewDumbStorage()
-
-	spawnEmployees(ms)
-	fmt.Println(ms.Get(3))
-
-	spawnEmployees(ds)
+	numbers := make(chan int)
+	go genrateNumbers(1000, numbers)
+	for n := range numbers {
+		fmt.Println(n)
+	}
 }
 
-func spawnEmployees(s storage.Storage) {
-	for i := 1; i <= 10; i++ {
-		s.Insert(storage.Employee{ID: i})
+func genrateNumbers(n int, res chan int) {
+	for i := 0; i <= n; i++ {
+		res <- i * 2
 	}
 }
